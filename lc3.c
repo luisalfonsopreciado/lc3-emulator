4289d9201bb0ba15c7b3c3bf4059fa277767880e
luisalfonsopreciado <luisapreciado99@gmail.com>
2021-05-17 08:42:56 -0500

    RTI instr implemented

--- lc3.c
+++ lc3.c
@@ -12,7 +12,6 @@
 #include <sys/termios.h>
 #include <sys/mman.h>
 
-#define R_COUNT 10
 #define FLAG_COUNT 3
 
 /* The LC-3 has 65,536 available memory locations */
@@ -31,6 +30,10 @@ enum registers
     R_R7,
     R_PC,   /* PROGRAM COUNTER (PC) */
     R_COND, /* CONDITION FLAG */
+    R_PSR,
+    R_SSP,   /* SUPERVISOR STACK POINTER */
+    R_USP,   /* USER STACK POINTER */
+    R_COUNT, /* NUMBER OF TOTAL REGISTERS */
 };
 
 uint16_t reg[R_COUNT];
@@ -86,16 +89,147 @@ enum
     MR_DDR = 0xFE06   /* display data*/
 };
 
+/* TODO: Load LC3 OS */
+void load_lc3_os()
+{
+        uint16_t trap_vector_table_entries[] = {0x0020, 0x0021, 0x0022, 0x0023, 0x0024, 0x0025};
+        uint16_t trap_vector_table_values[] = {0x0400, 0x0430, 0x0450, 0x04A0, 0x04E0, 0xFD70};
+    //     lc3os = {
+    //     // Implementation of GETC
+    //     0x0400: 0x3E07,
+    //     0x0401: 0xA004,
+    //     0x0402: 0x07FE,
+    //     0x0403: 0xA003,
+    //     0x0404: 0x2E03,
+    //     0x0405: 0xC1C0,
+    //     0x0406: 0xFE00,
+    //     0x0407: 0xFE02,
+    //     // Implementation of OUT
+    //     0x0430: 0x3E0A,
+    //     0x0431: 0x3208,
+    //     0x0432: 0xA205,
+    //     0x0433: 0x07FE,
+    //     0x0434: 0xB004,
+    //     0x0435: 0x2204,
+    //     0x0436: 0x2E04,
+    //     0x0437: 0xC1C0,
+    //     0x0438: 0xFE04,
+    //     0x0439: 0xFE06,
+    //     // Implementation of PUTS
+    //     0x0450: 0x3E16,
+    //     0x0451: 0x3012,
+    //     0x0452: 0x3212,
+    //     0x0453: 0x3412,
+    //     0x0454: 0x6200,
+    //     0x0455: 0x0405,
+    //     0x0456: 0xA409,
+    //     0x0457: 0x07FE,
+    //     0x0458: 0xB208,
+    //     0x0459: 0x1021,
+    //     0x045A: 0x0FF9,
+    //     0x045B: 0x2008,
+    //     0x045C: 0x2208,
+    //     0x045D: 0x2408,
+    //     0x045E: 0x2E08,
+    //     0x045F: 0xC1C0,
+    //     0x0460: 0xFE04,
+    //     0x0461: 0xFE06,
+    //     0x0462: 0xF3FD,
+    //     0x0463: 0xF3FE,
+    //     // Implementation of IN
+    //     0x04A0: 0x3E06,     // ST R7, SaveR7
+    //     0x04A1: 0xE006,     // LEA R0, Message
+    //     0x04A2: 0xF022,     // PUTS
+    //     0x04A3: 0xF020,     // GETC
+    //     0x04A4: 0xF021,     // OUT
+    //     0x04A5: 0x2E01,     // LD R7, SaveR7
+    //     0x04A6: 0xC1C0,     // RET
+    //     0x04A7: 0x3001,     // SaveR7 (.BLKW #1)
+    //     /* the "Input a character> " message goes here */
+    //     // Implementation of PUTSP
+    //     0x04E0: 0x3E27,
+    //     0x04E1: 0x3022,
+    //     0x04E2: 0x3222,
+    //     0x04E3: 0x3422,
+    //     0x04E4: 0x3622,
+    //     0x04E5: 0x1220,
+    //     0x04E6: 0x6040,
+    //     0x04E7: 0x0406,
+    //     0x04E8: 0x480D,
+    //     0x04E9: 0x2418,
+    //     0x04EA: 0x5002,
+    //     0x04EB: 0x0402,
+    //     0x04EC: 0x1261,
+    //     0x04ED: 0x0FF8,
+    //     0x04EE: 0x2014,
+    //     0x04EF: 0x4806,
+    //     0x04F0: 0x2013,
+    //     0x04F1: 0x2213,
+    //     0x04F2: 0x2413,
+    //     0x04F3: 0x2613,
+    //     0x04F4: 0x2E13,
+    //     0x04F5: 0xC1C0,
+    //     0x04F6: 0x3E06,
+    //     0x04F7: 0xA607,
+    //     0x04F8: 0x0801,
+    //     0x04F9: 0x0FFC,
+    //     0x04FA: 0xB003,
+    //     0x04FB: 0x2E01,
+    //     0x04FC: 0xC1C0,
+    //     0x04FE: 0xFE06,
+    //     0x04FF: 0xFE04,
+    //     0x0500: 0xF3FD,
+    //     0x0501: 0xF3FE,
+    //     0x0502: 0xFF00,
+    //     // Implementation of HALT
+    //     0xFD00: 0x3E3E,
+    //     0xFD01: 0x303C,
+    //     0xFD02: 0x2007,
+    //     0xFD03: 0xF021,
+    //     0xFD04: 0xE006,
+    //     0xFD05: 0xF022,
+    //     0xFD06: 0xF025,
+    //     0xFD07: 0x2036,
+    //     0xFD08: 0x2E36,
+    //     0xFD09: 0xC1C0,
+    //     0xFD70: 0x3E0E,
+    //     0xFD71: 0x320C,
+    //     0xFD72: 0x300A,
+    //     0xFD73: 0xE00C,
+    //     0xFD74: 0xF022,
+    //     0xFD75: 0xA22F,
+    //     0xFD76: 0x202F,
+    //     0xFD77: 0x5040,
+    //     0xFD78: 0xB02C,
+    //     0xFD79: 0x2003,
+    //     0xFD7A: 0x2203,
+    //     0xFD7B: 0x2E03,
+    //     0xFD7C: 0xC1C0,
+    //     /* the "halting the processor" message goes here */
+    //     0xFDA5: 0xFFFE,
+    //     0xFDA6: 0x7FFF,
+    //     // Display status register
+    //     0xFE04: 0x8000,
+    //     // Machine control register
+    //     0xFFFE: 0xFFFF,
+    // };
+}
+
 /* UNIX specific keyboard check */
 uint16_t check_key()
 {
     fd_set readfds;
+    /*FD_ZERO(&fdset) initializes a descriptor set fdset to the null set.*/
     FD_ZERO(&readfds);
-    FD_SET(STDIN_FILENO, &readfds);
+    /* FD_SET(fd, &fdset) : Include a particular descriptor fd in fdset (readfds) */
+    FD_SET(STDIN_FILENO, &readfds); /* include STDIN_FILENO in the readfds fd set */
 
+    /* maximum interval to wait for the below selection to complete */
     struct timeval timeout;
     timeout.tv_sec = 0;
     timeout.tv_usec = 0;
+
+    /* select call returns 1 if the readfds is ready for reading */
     return select(1, &readfds, NULL, NULL, &timeout) != 0;
 }
 
@@ -185,9 +319,12 @@ uint16_t mem_read(uint16_t addr)
 {
     if (addr == MR_KBSR)
     {
+        /* check if stdin has a key ready to be read */
         if (check_key())
         {
+            /* The ready bit (bit [15]) indicates if the keyboard has received a new character. */
             memory[MR_KBSR] = (1 << 15);
+            /* Place the character in the KeyBoard Data Register */
             memory[MR_KBDR] = getchar();
         }
         else
@@ -213,7 +350,7 @@ void print_mem(uint16_t start, uint16_t end)
     }
 }
 
-/* Update Flags */
+/* Update Flags given the register */
 void update_flags(uint16_t r)
 {
     if (reg[r] == 0)
@@ -279,9 +416,9 @@ The high eight bits of RO are cleared.
     {
         /*
             Write a string of ASCII characters to the console display. The characters are contained
-in consecutive memory locations, one character per memory location, starting with
-the address specified in RO. Writing terminates with the occurrence of xOOOO in a
-memory location.
+            in consecutive memory locations, one character per memory location, starting with
+            the address specified in RO. Writing terminates with the occurrence of xOOOO in a
+            memory location.
         */
 
         uint16_t *ptr = memory + reg[R_R0];
@@ -336,6 +473,8 @@ int main(int argc, char **argv)
         exit(2);
     }
 
+    load_lc3_os();
+
     /* Load the obj files into memory */
     for (int j = 1; j < argc; ++j)
     {
@@ -347,12 +486,15 @@ int main(int argc, char **argv)
     }
 
     /* Setup */
-    signal(SIGINT, handle_interrupt);
+    signal(SIGINT, handle_interrupt); /* handle the interrupt signal by calling the handle_interrupt function*/
     disable_input_buffering();
 
     /* By default the starting address for the LC-3 is 0x3000 */
     uint16_t PC_START = 0x3000;
     reg[R_PC] = PC_START;
+
+    /* PSR set initially to x8002*/
+    reg[R_PSR] = 0x8002;
 
     running = 1;
 
@@ -381,6 +523,13 @@ int main(int argc, char **argv)
         break;
         case OP_ADD:
         {
+            /*
+                If bit [5] is 0, the second source operand is obtained from SR2. If bit [5] is 1, the
+                second source operand is obtained by sign-extending the imm5 field to 16 bits.
+                In both cases, the second source operand is added to the contents of SRI and the
+                result stored in DR. The condition codes are set, based on whether the result is
+                negative, zero, or positive.
+            */
             uint16_t R_DR = (instr >> 9) & 0x7;
             uint16_t R_SR1 = (instr >> 6) & 0x7;
             /* are we in immediate mode? */
@@ -401,9 +550,10 @@ int main(int argc, char **argv)
         case OP_LD:
         {
             /* An address is computed by sign-extending bits [8:0] to 16 bits and adding this
-value to the incremented PC. The contents of memory at this address are loaded
-into DR. The condition codes are set, based on whether the value loaded is
-negative, zero, or positive. */
+            value to the incremented PC. The contents of memory at this address are loaded
+            into DR. The condition codes are set, based on whether the value loaded is
+            negative, zero, or positive. 
+            */
             uint16_t R_DEST = (instr >> 9) & 0x7;
             uint16_t PC_OFFSET = sign_extend(instr & 0x1FF, 9);
             reg[R_DEST] = mem_read(reg[R_PC] + PC_OFFSET);
@@ -412,9 +562,10 @@ negative, zero, or positive. */
         break;
         case OP_ST:
         {
-            /*The contents of the register specified by SR are stored in the memory location
-whose address is computed by sign-extending bits [8:0] to 16 bits and adding this
-value to the incremented PC.*/
+            /*  The contents of the register specified by SR are stored in the memory location
+                whose address is computed by sign-extending bits [8:0] to 16 bits and adding this
+                value to the incremented PC.
+            */
             uint16_t R_SR = (instr >> 9) & 0x7;
             uint16_t PC_OFFSET = sign_extend(0x1FF & instr, 9);
             mem_write(reg[R_PC] + PC_OFFSET, reg[R_SR]);
@@ -483,9 +634,43 @@ value to the contents of the register specified by bits [8:6].*/
         break;
         case OP_RTI:
         { /*If the processor is running in Supervisor mode, the top two elements on the
-Supervisor Stack are popped and loaded into PC, PSR. If the processor is running
-in User mode, a privilege mode violation exception occurs.*/
-            // TODO
+            Supervisor Stack are popped and loaded into PC, PSR. If the processor is running
+            in User mode, a privilege mode violation exception occurs.
+          */
+            // PSR[15] = 0 in Supervisor Mode
+            uint16_t sup_mode = (reg[R_PSR] >> 15) & 1;
+            if (sup_mode == 0)
+            {
+                // Pop PC from supervisor stack
+                reg[R_PC] = mem_read(reg[R_R6]);
+                reg[R_R6]++;
+                // Pop PSR from supervisor stack
+                reg[R_PSR] = mem_read(reg[R_R6]);
+                reg[R_R6]++;
+                // Restore User Stack Pointer
+                reg[R_R6] = reg[R_USP];
+            }
+            else
+            {
+                /* Privilege mode violation occurs if the processor encounters the RTI instruction while running in User Mode, throwing an illegal opcode exception */
+                // The processor sets the privilege mode to Supervisor mode (PSR[15] = 0).
+                reg[R_PSR] &= ~(1 << 15);
+                // R6 is loaded with the Supervisor Stack Pointer (SSP) if it does not already contain the SSP
+                reg[R_R6] = reg[R_SSP];
+                // The PSR and PC of the interrupted process are pushed onto the Supervisor Stack
+                mem_write(reg[R_R6]--, reg[R_PSR]);
+                mem_write(reg[R_R6]--, reg[R_PC]);
+                // The exception supplies its 8-bit vector. In the case of the Privilege mode violation, that vector is x00
+
+                // The processor expands that vector to x0100, the corresponding 16-bit address in the interrupt vector table.
+
+                /* The PC is loaded with the contents of memory location x0100,
+                the address of the first instruction in the corresponding exception service
+                routine. */
+                reg[R_PC] = 0x0100;
+                printf("illegal opcode exception: An RTI instruction was executed while in user mode. The machine has been halted.\n");
+                running = 0;
+            }
         }
 
         break;
